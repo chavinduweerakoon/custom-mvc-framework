40882f66fdedf938692827abed57c03fe8882366
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-09-12 04:25:54 +0530

    added routing in the urls

---
+++ app/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+class Pages
+{
+    public function __construct()
+    {
+    }
+    public function index()
+    {
+        echo 'home page';
+    }
+    public function about()
+    {
+        echo 'home about page';
+    }
+}
---
+++ app/require.php
@@ -0,0 +1,9 @@
+<?php
+//require libs from folder libraries
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
+
+
+//instatiate core class
+$init = new Core();

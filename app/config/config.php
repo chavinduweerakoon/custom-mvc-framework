e72f70d5209d445f9a804beba2600305ef60069a
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-09-12 07:06:35 +0530

    updated config with constants

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+//database params
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT
+define('URLROOT', 'localhost/custom-mvc-framework');
+
+//sitename
+define('SITENAME', 'MVC Framework');
--- app/require.php
+++ app/require.php
@@ -4,6 +4,6 @@ require_once 'libraries/Core.php';
 require_once 'libraries/Controller.php';
 require_once 'libraries/Database.php';
 
-
+require_once 'config/config.php';
 //instatiate core class
 $init = new Core();
